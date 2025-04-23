4227e75f189a8a6dab7d6bae66c4189ee4c53a2a
Ferenc Gyurkovics <ferenc.gyurkovics@codingo.hu>
2025-04-23 11:23:54 +0200

    chore: update install script

--- build_install.sh
+++ build_install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Building time-entry..."
+
 # Build the project
 go build -o time-entry ./cli
 
+echo "Installing time-entry..."
+
 # Install the project
 mv time-entry ~/go/bin/time-entry
+
+echo "Done!"
